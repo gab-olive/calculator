47730fc9dacb4cad391d772bf0e693d66fca3516
Gabriel <7272gabriel@gmail.com>
2025-01-11 11:09:11 -0300

    configured files and added basic functions

---
+++ index.js
@@ -0,0 +1,12 @@
+function add(n1,n2){
+    return n1+n2
+}
+function subtract(n1,n2){
+    return n1-n2
+}
+function multiply(n1,n2){
+    return n1*n2
+}
+function divide(n1,n2){
+    return n1/n2
+}
